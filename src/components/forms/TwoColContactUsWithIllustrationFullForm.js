b58d76b345a7066bb756255faa374889e757b301
JStruk <strukjustin@gmail.com>
2023-01-17 14:50:33 -0500

    format file

--- src/components/forms/TwoColContactUsWithIllustrationFullForm.js
+++ src/components/forms/TwoColContactUsWithIllustrationFullForm.js
@@ -25,7 +25,7 @@ const TextColumn = styled(Column)(props => [
 const PhoneNumber = tw.span`font-bold`
 
 const Image = styled.div(props => [
-    `background-image: url("${ props.imageSrc }");`,
+    `background-image: url("${props.imageSrc}");`,
     tw`rounded bg-contain bg-no-repeat bg-center h-full`,
 ]);
 const TextContent = tw.div`lg:py-8 text-center md:text-left`;
@@ -36,7 +36,7 @@ const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:t
 
 const Form = tw.form`mt-8 md:mt-10 text-sm flex flex-col max-w-sm mx-auto md:mx-0`
 const Input = tw.input`mt-6 first:mt-0 border-b-2 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500`
-const Textarea = styled(Input).attrs({as: "textarea"})` ${ tw`h-24` } `
+const Textarea = styled(Input).attrs({ as: "textarea" })` ${tw`h-24`} `
 const SelectWrapper = tw.div`flex justify-center items-center w-full my-8`
 const ExteriorSelectWrapper = tw.div`flex justify-center items-center w-full`
 const Select = tw.select`first:mt-0 border-b-2 py-3 mx-4 focus:outline-none font-medium w-full transition duration-300 hocus:border-primary-500 text-secondary-300`
@@ -46,30 +46,30 @@ const TextDiv = tw.div`text-secondary-100`
 const SubmitButton = tw(PrimaryButtonBase)`inline-block mt-8`
 
 export default ({
-                    subheading = "Contact Us",
-                    heading = <><span tw="text-primary-500">Get in touch </span>
-                        <wbr/>
-                        with us to book your appointment today!</>,
-                    description = "Give us a call or send us a message with the package you would like, preferred day(s) of the week and preferred time. We will be in touch to book your appointment.",
-                    phoneSection = <Description>Call us now at <PhoneNumber>(226) 749-4694</PhoneNumber> to schedule
-                        your appointment or contact us using the form below.</Description>,
-                    submitButtonText = "Send",
-                    formAction = "#",
-                    formMethod = "get",
-                    textOnLeft = true,
-                    links,
-                    interiorOptions = ['', 'Bronze (Basic interior detail)', 'Silver (Interior detail with shampoo)', 'Gold (Complete interior detail)'],
-                    exteriorOptions = ['', 'Bronze (Regular wash)', 'Silver (Wash and wax)', 'Gold (Wash, wax, and polish)', 'Platinum (Clay-bar, Carnauba wax)']
-                }) => {
-    const {register, reset, handleSubmit, formState: {errors}} = useForm();
+    subheading = "Contact Us",
+    heading = <><span tw="text-primary-500">Get in touch </span>
+        <wbr />
+        with us to book your appointment today!</>,
+    description = "Give us a call or send us a message with the package you would like, preferred day(s) of the week and preferred time. We will be in touch to book your appointment.",
+    phoneSection = <Description>Call us now at <PhoneNumber>(226) 749-4694</PhoneNumber> to schedule
+        your appointment or contact us using the form below.</Description>,
+    submitButtonText = "Send",
+    formAction = "#",
+    formMethod = "get",
+    textOnLeft = true,
+    links,
+    interiorOptions = ['', 'Bronze (Basic interior detail)', 'Silver (Interior detail with shampoo)', 'Gold (Complete interior detail)'],
+    exteriorOptions = ['', 'Bronze (Regular wash)', 'Silver (Wash and wax)', 'Gold (Wash, wax, and polish)', 'Platinum (Clay-bar, Carnauba wax)']
+}) => {
+    const { register, reset, handleSubmit, formState: { errors } } = useForm();
 
     const [interiorPackage, setInteriorPackage] = useState('')
     const [exteriorPackage, setExteriorPackage] = useState('')
 
     const getSubject = () => {
         let subject = ''
-        if(interiorPackage) subject += `Interior: ${interiorPackage.split('(')[0] || ''}  -  `
-        if(exteriorPackage) subject += `Exterior: ${exteriorPackage.split('(')[0] || ''}`
+        if (interiorPackage) subject += `Interior: ${interiorPackage.split('(')[0] || ''}  -  `
+        if (exteriorPackage) subject += `Exterior: ${exteriorPackage.split('(')[0] || ''}`
 
         return subject
     }
@@ -103,34 +103,34 @@ export default ({
     return (
         <>
             <AnimationRevealPage>
-                <Header links={ links }/>
+                <Header links={links} />
                 <Container>
                     <TwoColumn>
                         <ImageColumn>
-                            <Image imageSrc={ warchild } alt={ altText }/>
+                            <Image imageSrc={warchild} alt={altText} />
                         </ImageColumn>
-                        <TextColumn textOnLeft={ textOnLeft }>
+                        <TextColumn textOnLeft={textOnLeft}>
                             <TextContent>
-                                { subheading && <Subheading>{ subheading }</Subheading> }
-                                <Heading>{ heading }</Heading>
-                                { description && <Description>{ description }</Description> }
-                                { phoneSection }
-                                <Form action={ formAction } method={ formMethod } onSubmit={ handleSubmit(onSubmit) }>
+                                {subheading && <Subheading>{subheading}</Subheading>}
+                                <Heading>{heading}</Heading>
+                                {description && <Description>{description}</Description>}
+                                {phoneSection}
+                                <Form action={formAction} method={formMethod} onSubmit={handleSubmit(onSubmit)}>
                                     <Input type="text" name="from_name" placeholder="Full Name"
-                                           { ...register("from_name", {required: true}) }
+                                        {...register("from_name", { required: true })}
                                     />
-                                    { errors.from_name && <span>What's your name?</span> }
+                                    {errors.from_name && <span>What's your name?</span>}
 
                                     <Input type="email" name="reply_to" placeholder="Your Email Address"
-                                           { ...register("email", {required: true}) }
+                                        {...register("email", { required: true })}
                                     />
-                                    { errors.email && <span>Your Email is required</span> }
+                                    {errors.email && <span>Your Email is required</span>}
 
                                     <Input type="text" name="phone" placeholder="Phone Number"
-                                           { ...register("phone", {required: true}) }
+                                        {...register("phone", { required: true })}
                                     />
-                                    { errors.phone &&
-                                    <span> Let us know your phone number so we can get in touch ASAP </span> }
+                                    {errors.phone &&
+                                        <span> Let us know your phone number so we can get in touch ASAP </span>}
 
                                     <SelectWrapper>
                                         <TextDiv>
@@ -138,11 +138,11 @@ export default ({
                                         </TextDiv>
                                         <Select
                                             placeholder="Interior Package:"
-                                            onChange={ (e) => {setInteriorPackage(e.target.value)} }
+                                            onChange={(e) => { setInteriorPackage(e.target.value) }}
                                             value={interiorPackage}
                                         >Interior Package
-                                            { interiorOptions.map((option, index) => (
-                                                <Option key={index} value={ option } id={ option }> { option }</Option>)) }
+                                            {interiorOptions.map((option, index) => (
+                                                <Option key={index} value={option} id={option}> {option}</Option>))}
                                         </Select>
                                     </SelectWrapper>
 
@@ -156,28 +156,28 @@ export default ({
                                             onChange={(e) => { setExteriorPackage(e.target.value) }}
                                         >Exterior
                                             Package
-                                            { exteriorOptions.map((option, index) => (
-                                                <Option key={index} value={ option } id={ option }> { option }</Option>)) }
+                                            {exteriorOptions.map((option, index) => (
+                                                <Option key={index} value={option} id={option}> {option}</Option>))}
                                         </Select>
                                     </ExteriorSelectWrapper>
 
-                                    { errors.subject && <span>Briefly specify which package you'd like or the reason for
-                                your email</span> }
+                                    {errors.subject && <span>Briefly specify which package you'd like or the reason for
+                                        your email</span>}
 
                                     <Textarea name="message" placeholder="Your Message Here"
-                                              { ...register("message", {required: true}) }
+                                        {...register("message", { required: true })}
                                     />
-                                    { errors.message && <span>This field is required</span> }
-                                    <SubmitButton type="submit">{ submitButtonText }</SubmitButton>
+                                    {errors.message && <span>This field is required</span>}
+                                    <SubmitButton type="submit">{submitButtonText}</SubmitButton>
                                 </Form>
                             </TextContent>
                         </TextColumn>
                     </TwoColumn>
                 </Container>
-                <ToastContainer/>
-                <SEOComponent title="Contact Us" description="Contact us to book your appointment today!"/>
+                <ToastContainer />
+                <SEOComponent title="Contact Us" description="Contact us to book your appointment today!" />
             </AnimationRevealPage>
-            <Footer/>
+            <Footer />
         </>
     );
 }
